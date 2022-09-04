09c6ccd2462442505cf08085eefe21317e8a6b6d
AaronR92 <86726028+AaronR92@users.noreply.github.com>
2022-09-04 14:14:50 +0300

    Updated "/подкинуть" command
    Added more tanks

--- src/main/java/com/aaronr92/korben_chan_bot/command/FlipCoinCommand.java
+++ src/main/java/com/aaronr92/korben_chan_bot/command/FlipCoinCommand.java
@@ -1,5 +1,6 @@
 package com.aaronr92.korben_chan_bot.command;
 
+import com.aaronr92.korben_chan_bot.Emote;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -12,11 +13,20 @@ public class FlipCoinCommand extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        if (event.getName().equals("подкинуть")) {
-            if (random.nextInt(2) == 0) {
-                event.reply("Тебе выпал T95e6!").queue();
-            } else {
-                event.reply("Тебе выпал Объект 907!").queue();
+        if (event.getName().equals("открыть")) {
+            int randNum = random.nextInt(6);
+            if (randNum == 0) {
+                event.reply("Тебе выпал терпильный T95e6!").queue();
+            } else if (randNum == 1) {
+                event.reply("Тебе выпал Агент 907!").queue();
+            } else if (randNum == 2) {
+                event.reply("Тебе выпал достойный 279!").queue();
+            } else if (randNum == 3) {
+                event.reply("Тебе выпал легендарный т-22 ср.!").queue();
+            } else if (randNum == 4) {
+                event.reply(String.format("Тебе выпал лучший танк %s, VK 72.01 (K)!", Emote.JOVE.getCode())).queue();
+            } else if (randNum == 5) {
+                event.reply("Тебе выпал неповторимый Лев!").queue();
             }
         }
     }
